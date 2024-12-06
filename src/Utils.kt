ab35c385c744621a3ec5f9b1d1e2b6159d1cb0e3
seb <ciuisebi@gmail.com>
2024-12-06 19:30:57 +0200

    .

--- src/Utils.kt
+++ src/Utils.kt
@@ -29,8 +29,6 @@ data class Position(val x: Int, val y: Int) {
     operator fun plus(other: Position): Position = Position(this.x + other.x, this.y + other.y)
 }
 
-val INVALID_POSITION = Position(-1, -1)
-
 fun <T> List<List<T>>.get(position: Position): T = this[position.x][position.y]
 fun <T> List<MutableList<T>>.set(position: Position, newValue: T) {
     this[position.x][position.y] = newValue
