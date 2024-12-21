231d98c7487049ebf853c7577b2c562a8482036a
seb <ciuisebi@gmail.com>
2024-12-21 10:27:37 +0200

    🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄

--- src/utils/Grid.kt
+++ src/utils/Grid.kt
@@ -2,5 +2,8 @@ package utils
 
 open class Grid<T> (val map: MutableList<MutableList<T>>) {
     open fun get(position: Position): T = map[position.x][position.y]
+    open fun set(position: Position, t: T) {
+        map[position.x][position.y] = t
+    }
     open fun isInBounds(position: Position): Boolean = position.x in map.indices && position.y in map[0].indices
 }
